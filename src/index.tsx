4051f49f8ce33bab5df07fbb7c0dbd0453752908
Lloyd Ntim <lloyd.ntim@gmail.com>
2023-02-02 09:09:21 +0000

    removed uneccesary component and added color constants

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { render } from 'react-dom';
-import { ApolloProvider } from '@apollo/client';
+import {render} from 'react-dom';
+import {ApolloProvider} from '@apollo/client';
 
-import { createClient } from './graphql';
-import { AppTest } from './AppTest';
+import {createClient} from './graphql';
+import {App} from './App';
 
 import './localisation/i18n';
 
 /* eslint-disable no-undef */
 render(
   <ApolloProvider client={createClient()}>
-    <AppTest />
+    <App />
   </ApolloProvider>,
   document.getElementById('root'),
 );
